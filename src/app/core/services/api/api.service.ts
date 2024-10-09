197d5791ffe76fdc2db342e4e19497573d7dcb67
IvanKirik <ivankirikgavrilenko@gmail..com>
2024-10-09 09:50:18 +0300

    Fixed api service

--- src/app/core/services/api/api.service.ts
+++ src/app/core/services/api/api.service.ts
@@ -10,26 +10,26 @@ export class ApiService {
   private readonly http = inject(HttpClient);
   private readonly api_url = inject(API_URL);
 
-  get<T>(url: string, params: HttpParams = new HttpParams()): Observable<T> {
+  public get<T>(url: string, params: HttpParams = new HttpParams()): Observable<T> {
     return this.http.get<T>(`${this.api_url}${url}`, {
       headers: this.headers,
       params,
     });
   }
 
-  post<T, D>(url: string, data?: D): Observable<T> {
-    return this.http.post<T>(`${this.api_url}${url}`, JSON.stringify(data), {
+  public post<T, D>(url: string, data?: D): Observable<T> {
+    return this.http.post<T>(`${this.api_url}${url}`, data, {
       headers: this.headers,
     });
   }
 
-  patch<T, D>(url: string, data: D): Observable<T> {
-    return this.http.put<T>(`${this.api_url}${url}`, JSON.stringify(data), {
+  public patch<T, D>(url: string, data: D): Observable<T> {
+    return this.http.put<T>(`${this.api_url}${url}`, data, {
       headers: this.headers,
     });
   }
 
-  delete<T>(url: string): Observable<T> {
+  public delete<T>(url: string): Observable<T> {
     return this.http.delete<T>(`${this.api_url}${url}`, {
       headers: this.headers,
     });
